042fa9b6b9749c275d31ec0e963be865953dc29d
anupamadas31 <anupamadas31@gmail.com>
2020-09-26 20:56:57 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e8517d3d8ef0a674b61b.js"
+    "url": "webpack-runtime-d1140a87d5ca7f6a0013.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "85bcdf39a87184cde56b504b10c306e2"
+    "revision": "358a2fe342a83202aae0a4e06d2168c3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "a7207a41fd1cd2bf95d0b62b69705cf2"
+    "revision": "a50b59c7984eb5a0e8563d305f40d2b1"
   },
   {
     "url": "polyfill-d06486d78b0d3a46b835.js"
